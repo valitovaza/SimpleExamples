f7f65e0097ec8dcb117d8a71449a5e470cd71e9c
Azamat Valitov <valitov.azamat.m@gmail.com>
2018-11-25 20:04:14 +0500

    renaming AsyncTask

---
+++ Core/AsyncProcessor.swift
@@ -0,0 +1,28 @@
+public protocol AsyncProcessor {
+    func process()
+}
+public protocol AsyncTaskPerformer {
+    associatedtype TaskContent
+    func performAsyncTask(_ completion: @escaping (TaskContent)->())
+}
+public protocol AsyncTaskWaiter {
+    associatedtype TaskContent
+    func taskStarted()
+    func taskFinished(_ content: TaskContent)
+}
+public class AsyncProcessorImpl<Performer: AsyncTaskPerformer, TaskWaiter: AsyncTaskWaiter> where Performer.TaskContent == TaskWaiter.TaskContent {
+    private let performer: Performer
+    private let taskWaiter: TaskWaiter
+    init(_ performer: Performer, _ taskWaiter: TaskWaiter) {
+        self.performer = performer
+        self.taskWaiter = taskWaiter
+    }
+}
+extension AsyncProcessorImpl: AsyncProcessor {
+    public func process() {
+        taskWaiter.taskStarted()
+        performer.performAsyncTask { (content) in
+            self.taskWaiter.taskFinished(content)
+        }
+    }
+}
---
+++ CoreTests/AsyncProcessorTests.swift
@@ -0,0 +1,80 @@
+import XCTest
+
+class AsyncProcessorTests: XCTestCase {
+
+    private var sut: AsyncProcessorImpl<AsyncTaskPerformerSpy, AsyncTaskWaiterSpy>!
+    private var performer: AsyncTaskPerformerSpy!
+    private var taskWaiter: AsyncTaskWaiterSpy!
+    
+    override func setUp() {
+        super.setUp()
+        performer = AsyncTaskPerformerSpy()
+        taskWaiter = AsyncTaskWaiterSpy()
+        sut = AsyncProcessorImpl(performer, taskWaiter)
+    }
+    
+    override func tearDown() {
+        performer = nil
+        taskWaiter = nil
+        sut = nil
+        super.tearDown()
+    }
+    
+    func testProcessStartsAsyncTask() {
+        sut.process()
+        XCTAssertEqual(performer.performAsyncTaskCallCount, 1)
+    }
+    
+    func testProcessInvokesTaskStarted() {
+        sut.process()
+        XCTAssertEqual(taskWaiter.taskStartedCallCount, 1)
+        XCTAssertEqual(taskWaiter.taskFinishedCallCount, 0)
+    }
+    
+    func testProcessInvokesTaskStartedBeforePerformAsyncTask() {
+        performer.interceptionCallback = { [unowned self] in
+            XCTAssertEqual(self.taskWaiter.taskStartedCallCount, 1)
+        }
+        sut.process()
+    }
+    
+    func testTaskCompletionInvokesTaskFinished() {
+        sut.process()
+        performer.taskCompletion?(32)
+        XCTAssertEqual(taskWaiter.taskFinishedCallCount, 1)
+        XCTAssertEqual(taskWaiter.taskContent, 32)
+    }
+    
+    func testTaskCompletionDoesntInvokeTaskStarted() {
+        sut.process() //1
+        performer.taskCompletion?(0)
+        XCTAssertEqual(taskWaiter.taskStartedCallCount, 1)
+        XCTAssertEqual(performer.performAsyncTaskCallCount, 1)
+    }
+}
+extension AsyncProcessorTests {
+    class AsyncTaskPerformerSpy: AsyncTaskPerformer {
+        var performAsyncTaskCallCount = 0
+        var interceptionCallback: (()->())?
+        var taskCompletion: ((Int)->())?
+        func performAsyncTask(_ completion: @escaping (Int)->()) {
+            interceptionCallback?()
+            taskCompletion = completion
+            performAsyncTaskCallCount += 1
+        }
+    }
+    
+    class AsyncTaskWaiterSpy: AsyncTaskWaiter {
+        var taskStartedCallCount = 0
+        func taskStarted() {
+            taskStartedCallCount += 1
+        }
+        
+        var taskFinishedCallCount = 0
+        var taskContent: Int?
+        func taskFinished(_ content: Int) {
+            taskContent = content
+            taskFinishedCallCount += 1
+        }
+    }
+}
