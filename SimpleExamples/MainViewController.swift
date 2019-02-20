c288df22a06e7d60202b2c2a2e106d0da1caa2ea
Azamat Valitov <valitov.azamat.m@gmail.com>
2019-02-20 23:03:53 +0500

    Add rough solution to avoid unnecessary work during tests

--- SimpleExamples/MainViewController.swift
+++ SimpleExamples/MainViewController.swift
@@ -4,11 +4,18 @@ import HelloDependency
 
 class MainViewController: UIViewController {
     
+    private var isUnitTesting: Bool = {
+        ProcessInfo.processInfo.environment["XCTestConfigurationFilePath"] != nil
+    }()
+    private var isProdaction: Bool { return !isUnitTesting }
+    
     private var eventHandler: MainViewEventHandler!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        eventHandler = HelloDependency.resolve(MainViewEventHandler.self)
-        eventHandler.onLoad()
+        if isProdaction {
+            eventHandler = HelloDependency.resolve(MainViewEventHandler.self)
+            eventHandler.onLoad()
+        }
     }
 }
